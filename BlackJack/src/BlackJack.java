def2f26064d937193363fcf3e674e95f342e545a
Tristen6266 <159061732+Tristen6266@users.noreply.github.com>
2024-06-11 11:04:56 -0600

    imports

--- BlackJack/src/BlackJack.java
+++ BlackJack/src/BlackJack.java
@@ -1,14 +1,37 @@
 
-    import java.awt.*;
+import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.*;
 
 public class BlackJack {
+   
     private class Card {
         String value;
         String type;
 
+
+
+public String toString() {
+    return value + "-" + type;
+}
+
+public int getValue() {
+    if ("AJQK".contains(value)) { //A J Q K
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+    return Integer.parseInt(value); //2-10
+}
+public Boolean isAce(){
+    return value=="A";
+}
+public String getImage(){
+    
+
+}
 }
 }
---
+++ src/BlackJack.java
@@ -0,0 +1,14 @@
+
+    import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+}
+}
